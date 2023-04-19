4864858cdb39a6ca83f3c4e6dc621fcd7277f347
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-19 13:52:01 -0700

    Add method for filtering traver by date

--- src/classes/Traveler.js
+++ src/classes/Traveler.js
@@ -1,23 +1,37 @@
+const dayjs = require("dayjs");
+import Trip from "./Trip";
+
 class Traveler {
     constructor(data) {
         this.id = data.id;
         this.name = data.name;
         this.travelerType = data.travelerType;
-        this.trips = []
+        this.trips = [];
     }
 
-    findAllTripsByTraveler(tripsData) {
-        return this.trips = tripsData.filter(trip => this.id === trip.userID)
+    findAllTripsByTraveler(tripData) {
+        const filteredTrips = tripData.filter((trip) => this.id === trip.userID);
+        this.trips = filteredTrips.map(trip => new Trip(trip))
+        return this.trips
     }
 
-    returnTravelersFirstName(){
-        return this.name.split(" ")[0]
+    returnTravelersFirstName() {
+        return this.name.split(" ")[0];
+    }
+
+    filterTripsByStatus(status) {
+        return this.trips.filter((trip) => trip.status === status)
+    }
+
+    filterUpcomingTrips(date) {
+        return this.trips.filter((trip) => { 
+           return (dayjs(trip.date).format("YYYY/MM/DD") >= dayjs(date).format("YYYY/MM/DD"));
+        });
     }
 
     // calculateTotalCost(tripsData, destinationData) {
     //     this.findAllTripsByTraveler(tripsData)
     // }
-
 }
 
 export default Traveler;
--- src/scripts.js
+++ src/scripts.js
@@ -3,10 +3,14 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import Traveler from './classes/Traveler';
+import Trip from './classes/Trip';
+
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 import './images/logo-main.png';
 
-
+const dayjs = require("dayjs");
+console.log(dayjs)
 console.log('This is the JavaScript entry file - your code begins here.');
