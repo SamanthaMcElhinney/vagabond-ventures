8147139b5e866e633a8945649591105125d911a5
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-19 15:54:53 -0700

    Add destination repo

--- src/classes/Destination-Repo.js
+++ src/classes/Destination-Repo.js
@@ -1,7 +1,19 @@
-class DestinationRep {
-    constructor(destinationData){
+class DestinationRepository {
+    constructor(destinationData, tripData){
         this.destinations = destinationData
+        this.trips = tripData
     };
+
+    findDestinationById(id) {
+        const destination = this.destinations.find(destination => destination.id === id)
+        return destination
+    }
+
+    findDestinationByName(name){
+        const destination = this.destinations.find(destination => destination.destination === name)
+        return destination
+    }
+    //calcualte trip cost..then connect it to the user's trip cost
 }
 
-export default DestinationRep;
+export default DestinationRepository;
