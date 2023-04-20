fb9b901800e9222e5464bc2ef33cf3eaecff0dce
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-20 15:01:04 -0700

    Add traveler repo to hold all the travelers with method to find traveler by id

--- src/classes/Traveler-Repo.js
+++ src/classes/Traveler-Repo.js
@@ -1,5 +1,3 @@
-const dayjs = require("dayjs");
-
 class TravelerRepository {
   constructor(travelersData) {
     this.data = travelersData;
@@ -8,39 +6,6 @@ class TravelerRepository {
   getSingleTravelerById(id) {
     return this.data.find((traveler) => traveler.id === id);
   }
-
-//   filterTripsByStatus(status) {
-//     return this.trips.filter((trip) => trip.status === status);
-//   }
-
-//   filterPastTrips(date) {
-//     return this.trips.filter((trip) => {
-//       return (
-//         dayjs(trip.date).format("YYYY/MM/DD") <=
-//         dayjs(date).format("YYYY/MM/DD")
-//       );
-//     });
-//   }
-
-//   filterUpcomingTrips(date) {
-//     return this.trips.filter((trip) => {
-//       return (
-//         dayjs(trip.date).format("YYYY/MM/DD") >=
-//         dayjs(date).format("YYYY/MM/DD")
-//       );
-//     });
-//   }
-
-//   calculateTotalSpentByTraveler(tripData, destinationData) {
-//     let trips = this.findAllTripsByTraveler(tripData);
-//     return trips.reduce((acc, trip) => {
-//       acc +=
-//         trip.calculateAgentFee(destinationData) +
-//         trip.calculateTripCost(destinationData);
-//       console.log(acc, "acc");
-//       return acc;
-//     }, 0);
-//   }
 }
 
 export default TravelerRepository;
