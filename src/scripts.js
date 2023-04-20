2788325d3b56498ea22e3cf8e485b6a18830578b
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-20 12:42:54 -0700

    Add functionality to display past trips to DOM and total spent by user

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,7 @@ import './css/styles.css';
 // Classes import
 import Traveler from './classes/Traveler';
 import Trip from './classes/Trip';
+import DestinationRepository from './classes/Destination-Repo';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
@@ -19,13 +20,15 @@ console.log(dayjs)
 
 //query selectors:
 const pastTripsButton = document.querySelector("#pastTripsButton");
-const usersCard = document.querySelector("#usersCard")
+const usersCard = document.querySelector("#cardGrid")
+const usersStats = document.querySelector(".navigation-user-card")
 
 let allTrip;
 let currentTraveler;
 let allDestinations;
+let currentTravelerTrips
 
-let date = dayjs("2020/03/01").format("YYYY/MM/DD");
+let date = dayjs().format("YYYY/MM/DD");
 console.log(date, "date")
 console.log('This is the JavaScript entry file - your code begins here.');
 
@@ -36,17 +39,17 @@ import {
   fetchAllTrips,
   fetchAllDestinations
 } from './ApiCalls';
-import DestinationRepository from './classes/Destination-Repo';
 
 Promise.all([fetchTravelers(), fetchAllTrips(), fetchAllDestinations()])
   .then(
     ([travelerData, tripData, destinationData]) => {
       allTrip = tripData
-      allDestinations = new DestinationRepository(destinationData)
-      console.log(allDestinations, "allDest")
+      allDestinations = destinationData
       currentTraveler = new Traveler(travelerData[1]);
-      currentTraveler.findAllTripsByTraveler(allTrip)
+      currentTravelerTrips = currentTraveler.findAllTripsByTraveler(allTrip);
+      console.log(currentTravelerTrips, "current traveler trips")
       displayUser(currentTraveler);
+      renderTotalSpent(currentTraveler, currentTravelerTrips, allDestinations);
     }
   );
 
@@ -57,22 +60,41 @@ const displayUser = (currentTraveler) => {
       <p class="navigation-stats-sent">Here are your stats</p>`;
 }
 
+
 pastTripsButton.addEventListener("click", () => {
   renderPastTrips(currentTraveler, date);
 })
 
+function renderTotalSpent(
+  currentTraveler,
+  currentTravelerTrips,
+  allDestinations
+) {
+  usersStats.innerHTML = `<div class="navigation-user-card">
+        <h2 class="navigation-user-welcome">Welcome Back </h2>
+        <p class="navigation-stats-sent">Here are your stats</p>
+        <p class="navigation-stats-invested">You have invested $ ${currentTraveler.calculateTotalSpentByTraveler(
+          currentTravelerTrips,
+          allDestinations
+        )} in your happiness</p>
+      </div>`;
+}
+
 const renderPastTrips = (currentTraveler, date) => {
   let trips = currentTraveler.filterPastTrips(date)
   usersCard.innerHTML = " "
-  trips.forEach((trip)=> {
+  trips.forEach((trip) => {
     usersCard.innerHTML += `
-     <h4 card-title></h4>
-            <img class="card-holder-img" src="" alt="" alt="">
-            <div class=card-text>
-              <p class="card-travelers"><b>Travelers: ${trip.travelers}</b></p>
-              <p class="card-start-date"><b>Start Date: ${trip.date}</b></p>
-              <p class="card-duration"><b>Duration: ${trip.duration}</b></p>
-              <p class="card-status"><b>Status: ${trip.status}</b></p>
+    <section class="card-grid" id="cardGrid">
+    <section class="card" id="usersCard">
+    <h4 card-title></h4>
+    <img class="card-holder-img" src="" alt="" alt="">
+    <div class=card-text>
+    <p class="card-travelers"><b>Travelers: ${trip.travelers}</b></p>
+    <p class="card-start-date"><b>Start Date: ${trip.date}</b></p>
+    <p class="card-duration"><b>Duration: ${trip.duration}</b></p>
+    <p class="card-status"><b>Status: ${trip.status}</b></p>
     `;
   })
-};
+  
+}
