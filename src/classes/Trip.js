8790cd94166113b5c9489ba079da413233404949
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-19 17:12:13 -0700

    create method for calculating trip cost and agent fee

--- src/classes/Trip.js
+++ src/classes/Trip.js
@@ -1,3 +1,5 @@
+import DestinationRepository from "./Destination-Repo";
+
 class Trip {
   constructor(tripInfo) {
     this.id = tripInfo.id;
@@ -10,9 +12,20 @@ class Trip {
     this.suggestedActivities = tripInfo.suggestedActivities;
   };
 
-  calculateTripCost(){
-    
-  }
+  //once we find the id of the destination that matches the id of the trip. then we can calculate the total cost
+
+  calculateTripCost(destinationData){
+    const destination = destinationData.find(destination => destination.id === this.destinationID)
+    const total =
+      (this.travelers * destination.estimatedFlightCostPerPerson) +
+      (this.travelers * destination.estimatedLodgingCostPerDay)
+    return total
+}
+calculateAgentFee(destinationData){
+  const tripCost = this.calculateTripCost(destinationData)
+  const agentFee = (tripCost * .10).toFixed(2) 
+  return Number(agentFee)
 }
 
+}
 export default Trip;
