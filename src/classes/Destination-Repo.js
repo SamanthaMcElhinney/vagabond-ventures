249fd4ccc03fbd8bb6142db867c657cb75e3ce15
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-20 16:39:57 -0700

    Add method for calcuating total including agent fee

--- src/classes/Destination-Repo.js
+++ src/classes/Destination-Repo.js
@@ -1,7 +1,30 @@
 class DestinationRepository {
-    constructor(destinationData){
-        this.destinations = destinationData
-    };
+  constructor(destinationData) {
+    this.data = destinationData;
+  }
+
+  getSingleDestinationById(id) {
+    return this.data.find((destination) => destination.id === id);
+  }
+
+  findDestinationByName(name) {
+    const destination = this.data.find(
+      (destination) => destination.destination === name
+    );
+    if (!destination) {
+      return "Sorry no such destination";
+    } else {
+      return destination;
+    }
+  }
+
+  calculateTotalTripCost(id, travelers, duration) {
+    let destination = this.getSingleDestinationById(id);
+    let totalCost =
+      (destination.estimatedLodgingCostPerDay * duration) +
+      (destination.estimatedFlightCostPerPerson * travelers)
+    return Number((totalCost * 1.1).toFixed(2));
+  }
 }
 
 export default DestinationRepository;
