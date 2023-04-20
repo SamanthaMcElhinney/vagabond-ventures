641e998fdcf8f784b4dde82e372935e5eb346789
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-19 17:11:44 -0700

    Create method for filtering trips by status and date

--- src/classes/Traveler.js
+++ src/classes/Traveler.js
@@ -2,36 +2,39 @@ const dayjs = require("dayjs");
 import Trip from "./Trip";
 
 class Traveler {
-    constructor(data) {
-        this.id = data.id;
-        this.name = data.name;
-        this.travelerType = data.travelerType;
-        this.trips = [];
-    }
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.travelerType = data.travelerType;
+    this.trips = [];
+  }
 
-    findAllTripsByTraveler(tripData) {
-        const filteredTrips = tripData.filter((trip) => this.id === trip.userID);
-        this.trips = filteredTrips.map(trip => new Trip(trip))
-        return this.trips
-    }
+  findAllTripsByTraveler(tripData) {
+    const filteredTrips = tripData.filter((trip) => this.id === trip.userID);
+    this.trips = filteredTrips.map((trip) => new Trip(trip));
+    return this.trips;
+  }
 
-    returnTravelersFirstName() {
-        return this.name.split(" ")[0];
-    }
+  returnTravelersFirstName() {
+    return this.name.split(" ")[0];
+  }
 
-    filterTripsByStatus(status) {
-        return this.trips.filter((trip) => trip.status === status)
-    }
+  filterTripsByStatus(status) {
+    return this.trips.filter((trip) => trip.status === status);
+  }
 
-    filterUpcomingTrips(date) {
-        return this.trips.filter((trip) => { 
-           return (dayjs(trip.date).format("YYYY/MM/DD") >= dayjs(date).format("YYYY/MM/DD"));
-        });
-    }
+  filterUpcomingTrips(date) {
+    return this.trips.filter((trip) => {
+      return (
+        dayjs(trip.date).format("YYYY/MM/DD") >=
+        dayjs(date).format("YYYY/MM/DD")
+      );
+    });
+  }
+// Traveler: Calculate Total Spent on Trips by Traveler
+//match trip id to destination id
+//add total 
 
-    // calculateTotalCost(tripsData, destinationData) {
-    //     this.findAllTripsByTraveler(tripsData)
-    // }
 }
 
 export default Traveler;
