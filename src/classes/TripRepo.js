ac1d6fb8f46b329cb04d7b3fb15d374bbb2d2f5a
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-20 22:09:26 -0700

    Add structural elements for accessibility check and ids for buttons

--- src/classes/TripRepo.js
+++ src/classes/TripRepo.js
@@ -22,17 +22,35 @@ class TripRepo {
       (trip) =>
         dayjs(trip.date).format("YYYY/MM/DD") >=
         dayjs(date).format("YYYY/MM/DD")
-    )
+    );
     if (results.length === 0) {
-      return "Sorry Friend! You don't have any upcoming trips"
+      return "Sorry Friend! You don't have any upcoming trips";
     }
   }
 
   returnPendingTrips(id) {
-    const result = this.findAllTripsByTraveler(id).filter(trip => trip.status === 'pending')
-    console.log(result)
-    return result
+    const result = this.findAllTripsByTraveler(id).filter(
+      (trip) => trip.status === "pending"
+    );
+    console.log(result);
+    return result;
   }
-  
+  getTripsByStatus(id, status) {
+    let travelerTrips = this.getTripsByUserId(id);
+    return travelerTrips.filter((trip) => trip.status === status);
+  }
+  calculateTotalSpentByTraveler(id, destinations){
+    let trips = this.findAllTripsByTraveler(id)
+    let total = trips.reduce((acc, trip) => {
+      acc += destinations.calculateTotalTripCost(
+        trip.destinationID,
+        trip.travelers,
+        trip.duration
+      )
+      console.log(acc)
+      return acc
+    },0)
+    return total
+  }
 }
 export default TripRepo;
