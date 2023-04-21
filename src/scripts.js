006f92537a3a37ab5162bb1cf2b44bcb32b657fa
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-20 18:24:22 -0700

    All functionality to display user greeting and total spent to the page on page load and add functionality to display past trips after clicking on button

--- src/scripts.js
+++ src/scripts.js
@@ -2,87 +2,80 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
+import "./css/styles.css";
 
 // Classes import
-import Traveler from './classes/Traveler';
-import Trip from './classes/Trip';
-import DestinationRepository from './classes/Destination-Repo';
+import DestinationRepository from "./classes/Destination-Repo";
+import TravelerRepository from "./classes/Traveler-Repo";
+import Traveler from "./classes/Traveler";
+import TripRepo from "./classes/TripRepo";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
-import './images/logo-main.png';
-import './images/rainbow.jpg';
+import "./images/turing-logo.png";
+import "./images/logo-main.png";
+import "./images/rainbow.jpg";
 
 //third party library imports
 const dayjs = require("dayjs");
-console.log(dayjs)
+console.log(dayjs);
 
 //query selectors:
 const pastTripsButton = document.querySelector("#pastTripsButton");
-const usersCard = document.querySelector("#cardGrid")
-const usersStats = document.querySelector(".navigation-user-card")
+const usersCard = document.querySelector("#cardGrid");
+const usersStats = document.querySelector(".navigation-user-card");
+const userCard = document.querySelector(".navigation-user-card");
 
-let allTrip;
-let currentTraveler;
+//Global variables
+let allTrips;
+let allTravelers;
 let allDestinations;
-let currentTravelerTrips
+let currentTraveler;
 
 let date = dayjs().format("YYYY/MM/DD");
-console.log(date, "date")
-console.log('This is the JavaScript entry file - your code begins here.');
+console.log(date, "date");
+console.log("This is the JavaScript entry file - your code begins here.");
 
 // import API Calls
-
 import {
   fetchTravelers,
   fetchAllTrips,
-  fetchAllDestinations
-} from './ApiCalls';
+  fetchAllDestinations,
+} from "./ApiCalls";
 
-Promise.all([fetchTravelers(), fetchAllTrips(), fetchAllDestinations()])
-  .then(
-    ([travelerData, tripData, destinationData]) => {
-      allTrip = tripData
-      allDestinations = destinationData
-      currentTraveler = new Traveler(travelerData[1]);
-      currentTravelerTrips = currentTraveler.findAllTripsByTraveler(allTrip);
-      console.log(currentTravelerTrips, "current traveler trips")
-      displayUser(currentTraveler);
-      renderTotalSpent(currentTraveler, currentTravelerTrips, allDestinations);
-    }
-  );
+Promise.all([fetchTravelers(), fetchAllTrips(), fetchAllDestinations()]).then(
+  ([travelerData, tripData, destinationData]) => {
+    allTravelers = new TravelerRepository(travelerData)
+    allDestinations = new DestinationRepository(destinationData)
+    allTrips = new TripRepo(tripData)
+    currentTraveler = new Traveler(allTravelers.getSingleTravelerById(2));
+    displayUser(currentTraveler, allDestinations, allTrips);
+  }
+);
 
-const displayUser = (currentTraveler) => {
-  const userCard = document.querySelector(".navigation-user-card");
-  userCard.innerHTML = ` 
-     <h2 class="navigation-user-welcome">Welcome Back ${currentTraveler.name}, you crazy ${currentTraveler.travelerType}</h2>
-      <p class="navigation-stats-sent">Here are your stats</p>`;
-}
-
-
-pastTripsButton.addEventListener("click", () => {
-  renderPastTrips(currentTraveler, date);
-})
-
-function renderTotalSpent(
+const displayUser = (
   currentTraveler,
-  currentTravelerTrips,
-  allDestinations
-) {
-  usersStats.innerHTML = `<div class="navigation-user-card">
-        <h2 class="navigation-user-welcome">Welcome Back </h2>
-        <p class="navigation-stats-sent">Here are your stats</p>
-        <p class="navigation-stats-invested">You have invested $ ${currentTraveler.calculateTotalSpentByTraveler(
-          currentTravelerTrips,
+  allDestinations,
+  allTrips
+) => {
+  userCard.innerHTML = ` <div class="navigation-user-card">
+     <h2 class="navigation-user-welcome">Welcome back ${currentTraveler.returnTravelersFirstName()}!</h2>
+     <p>I love that you are a ${currentTraveler.travelerType} ✨</p>
+     <p class="navigation-stats-sent">Here are your stats:</p>
+        <p class="navigation-stats-invested">You have invested $ ${allTrips.calculateTotalSpentByTraveler(
+          currentTraveler.id,
           allDestinations
         )} in your happiness</p>
       </div>`;
-}
+};
 
-const renderPastTrips = (currentTraveler, date) => {
-  let trips = currentTraveler.filterPastTrips(date)
-  usersCard.innerHTML = " "
+pastTripsButton.addEventListener("click", () => {
+  renderPastTrips(allTrips, currentTraveler, date);
+});
+
+
+const renderPastTrips = (allTrips, currentTraveler, date) => {
+  let trips = allTrips.returnPastTrips(currentTraveler.id, date)
+  usersCard.innerHTML = " ";
   trips.forEach((trip) => {
     usersCard.innerHTML += `
     <section class="card-grid" id="cardGrid">
@@ -95,6 +88,5 @@ const renderPastTrips = (currentTraveler, date) => {
     <p class="card-duration"><b>Duration: ${trip.duration}</b></p>
     <p class="card-status"><b>Status: ${trip.status}</b></p>
     `;
-  })
-  
-}
+  });
+};
