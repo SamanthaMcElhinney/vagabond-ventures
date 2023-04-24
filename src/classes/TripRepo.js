18014f556412d3de8e5d9f481a0a358ff579ef04
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-23 17:23:53 -0700

    Refactor and delete unused methods

--- src/classes/TripRepo.js
+++ src/classes/TripRepo.js
@@ -1,14 +1,13 @@
 const dayjs = require("dayjs");
-import DestinationRepository from "./Destination-Repo";
 
 class TripRepo {
   constructor(tripInfo) {
     this.data = tripInfo;
-  }
+  };
 
   findAllTripsByTraveler(id) {
     return this.data.filter((data) => data.userID === id);
-  }
+  };
 
   returnPastTrips(id, date) {
     return this.findAllTripsByTraveler(id).filter(
@@ -16,7 +15,7 @@ class TripRepo {
       trip.status === 'approved' && (dayjs(trip.date).format("YYYY/MM/DD") <=
         dayjs(date).format("YYYY/MM/DD"))
     );
-  }
+  };
 
   returnFutureTrips(id, date) {
     const result = this.findAllTripsByTraveler(id).filter(
@@ -25,20 +24,20 @@ class TripRepo {
       dayjs(trip.date).format("YYYY/MM/DD") >=
       dayjs(date).format("YYYY/MM/DD")
     );
-    return result 
-  }
+    return result;
+  };
 
   returnPendingTrips(id) {
     const result = this.findAllTripsByTraveler(id).filter(
       (trip) => trip.status === "pending"
-    )
+    );
     return result;
-  }
+  };
 
   getTripsByStatus(id, status) {
     let travelerTrips = this.getTripsByUserId(id);
     return travelerTrips.filter((trip) => trip.status === status);
-  }
+  };
 
   calculateTotalSpentByTraveler(id, destinations) {
     let trips = this.findAllTripsByTraveler(id)
@@ -47,25 +46,12 @@ class TripRepo {
         trip.destinationID,
         trip.travelers,
         trip.duration
-      )
-      return acc
-    }, 0)
-    return total
-  }
-  }
-
-// fi(id, destinationsData){
-//   let tripsById = this.findAllTripsByTraveler(3);
-//  let destinationArray = destinationsData.data.forEach(destination => {
-//     tripsById.reduce((acc, currentTrip) => {
-//       console.log(currentTrip, "CT")
-//         // if(destination.id === currentTrip.destinationID) {
-//         //   console.log(currentTrip, "CT")
-//         //   acc.push(destination)
-//         return acc
-//       })
-//     },[])
-//     return destinationArray
-//   } 
+      );
+      return acc;
+    }, 0);
+    return total;
+  };
+}
+;
 
 export default TripRepo;
